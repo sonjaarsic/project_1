e8232e9250a9b7ec924cb72dd125c23d6c19ac90
sonjaarsic <85966007+sonjaarsic@users.noreply.github.com>
2021-06-16 00:29:28 +0200

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,23 @@
-touch README.md
-echo "# Title of my README.md\n" >> README.md
-echo "Date and time:  " >> README.md
-date >> README.md
-echo "" >> README.md
-echo "Number of lines in guessinggame.sh:  " >> README.md
-less guessinggame.sh | wc -l >> README.md
+#!/usr/bin/env bash
+
+clear
+declare -i numFiles
+numFiles=$(find . -maxdepth 1 -not -type d | wc --lines)
+
+function makeValidGuess {
+	until read -p "Enter your guess:  " guess;
+	[[ "$guess" =~ ^[0-9]+$ ]]; do
+		echo "\nEnter unnegative number!\n"
+	done
+}
+
+while makeValidGuess; do
+	if (( guess < numFiles )); then
+		echo "\nYour guess is to low."
+	elif (( guess > numFiles )); then
+		echo"\nYour guess is too high."
+	else
+		echo "\nCongratulations! Answer $numFiles is correct!"
+		exit
+	fi
+done
