34f6f8c4a4015f3c346bc7be3634e845e6c81e95
sonjaarsic <85966007+sonjaarsic@users.noreply.github.com>
2021-06-16 10:17:48 +0200

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,20 +5,20 @@ clear
 declare -i numFiles
 numOfFiles=$(find . -maxdepth 1 -not -type d | wc --lines)
 
-function makeValidGuess {
+function guessFun {
 	until read -p "Enter your guess:  " guess;
 	[[ "$guess" =~ ^[0-9]+$ ]]; do
 		echo "\nEnter unnegative number!\n"
 	done
 }
 
-while makeValidGuess; do
+while guessFun; do
 	if [[ $guess -lt $numberOfFiles ]]; then
 		echo "Your guess is to low. Type a new one."
 	elif (( guess > numFiles )); then
 		echo"Your guess is too high.Type a new one"
 	else
-		echo "Congratulations! Answer $numOfFiles is correct!"
+		echo "Congratulations! $numOfFiles is correct answer!"
 		exit
 	fi
 done
