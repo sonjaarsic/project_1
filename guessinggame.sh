d3eae4dac17da5ddd61374e0b64f7c9b730ede06
sonjaarsic <miraarsic70@yahoo.com>
2021-06-16 14:06:51 +0200

    secguess

---
+++ guessinggame.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# File:guessinggame.sh
+
+clear
+declare -i numOfFiles
+numOfFiles=$(find . -maxdepth 1 -not -type d | wc --l)
+
+echo "Guess the number of files in current directory. "
+
+function guessFun {
+	until read -p "Enter your guess:  " guess;
+	[[ "$guess" =~ ^[0-9]+$ ]]; do
+		echo "\nEnter unnegative number!\n"
+	done
+}
+
+while guessFun; do
+	if [[ $guess -lt $numOfFiles ]]; then
+		echo "Your guess is to low. Type a new one."
+	elif (( guess > numOfFiles )); then
+		echo "Your guess is too high.Type a new one"
+	else
+		echo "Congratulations! $numOfFiles is correct answer!"
+		exit
+	fi
+done
